38671723c6cb4f3932ba69058f9fb73b3968783b
rayzheng59 <rayzheng59@gmail.com>
2023-11-03 00:15:09 +0800

    login works

--- Sc2002Assignment/src/BE/Server.java
+++ Sc2002Assignment/src/BE/Server.java
@@ -6,18 +6,27 @@ import java.io.IOException;
 //put it inside MainPage,so when run menu,it will generate out data to compare
 import java.util.ArrayList;
 
-public class server {
+public class Server {
 	
 	public static String StudentFilePath = "/Users/cheongray/Datasc2002/student";
-	String StaffFilePath = "/Users/cheongray/Datasc2002/staff";//haven't put yet
+	public static String StaffFilePath = "/Users/cheongray/Datasc2002/staff";//haven't put yet
+	private static ArrayList<String> names;
+    private static ArrayList<String> emails;
+    private static ArrayList<String> faculties;
+    private static ArrayList<String> userIDs;
+    private static ArrayList<User> usersData;
+    public Server()
+    {
+    	start();
+    }
 	public static void start()
 	{
 		ArrayList<ArrayList<String>> dataFromFileHandler = new ArrayList<>();
-		ArrayList<String> names = new ArrayList<>();
-        ArrayList<String> emails = new ArrayList<>();
-        ArrayList<String> faculties = new ArrayList<>();
-        ArrayList<String> userIDs= new ArrayList<>();
-        ArrayList<User> usersData = new ArrayList<>();
+		names = new ArrayList<>();
+        emails = new ArrayList<>();
+        faculties = new ArrayList<>();
+        userIDs = new ArrayList<>();
+        usersData = new ArrayList<>();
 
 		try {
             dataFromFileHandler=File_Handler.PutFileContentIntoArray(StudentFilePath);
@@ -31,35 +40,36 @@ public class server {
                 User user = new User(names.get(i), userIDs.get(i), faculties.get(i));
                 usersData.add(user);
             }
-            for (User user : usersData) {
-                System.out.println("Name: " + user.getName());
-                System.out.println("NTU Network ID: " + user.getNtuNetworkId());
-                System.out.println("Faculty: " + user.getFaculty());
-                System.out.println("password:" + user.getPassword());
-                System.out.println("--------");
-            }
-            
-            
-//            for (int i = 0; i < names.size(); i++) {
-//                System.out.println("Name: " + names.get(i));
-//                System.out.println("Email: " + emails.get(i));
-//                System.out.println("Faculty: " + faculties.get(i));
-//                System.out.println("UserID: " + userIDs.get(i));
+//            for (User user : usersData) {
+//                System.out.println("Name: " + user.getName());
+//                System.out.println("NTU Network ID: " + user.getNtuNetworkId());
+//                System.out.println("Faculty: " + user.getFaculty());
+//                System.out.println("password:" + user.getPassword());
 //                System.out.println("--------");
 //            }
+//            
+        
 //            
         } catch(IOException e){
 //   		 System.err.println("An error occurred while reading the file: " + e.getMessage());
 //   	}
 	}
 	}
+	public User login(String userID, String password) {
+        for (User user : usersData) {
+            if (user.getNtuNetworkId().equals(userID) && user.getPassword().equals(password)) {
+                return user; // Successful login
+            }
+        }
+        return null; // Login failed
+    }
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		server.start();
+		Server.start();
 	}
 
 }
--- Sc2002Assignment/src/FE/MainPage.java
+++ Sc2002Assignment/src/FE/MainPage.java
@@ -1,23 +1,35 @@
 package FE;
 import java.util.Scanner;
-
-//remember to use server(BE) to generate the server(server will just be arrays storing details)
+import BE.Server;
+import BE.User;
+//remember to use server(BE) to generate the server(server will just be arraylist of Users  storing details)
 public class MainPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 Scanner scanner = new Scanner(System.in);
-	        boolean loggedIn = false;
-	        String userId = "";
-
-	        while (true) {
-	            if (!loggedIn) {
-	            	
-	            }
-	        
-	            else {
-	            	
-	            }
+	private static Server serverInstance;
+	public MainPage() {
+        //new instance of server for every new page
+		serverInstance = new Server();
+    }
+	public static void main(String[] args) 
+	{
+		MainPage M = new MainPage();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("-------------Camp Application and Management System (CAMs).----------------");
+		System.out.println("---------------------------------------------------------------------------");
+		System.out.println("UserID:");
+		String UserID= scanner.nextLine();
+		System.out.println("Password:");
+		String Password= scanner.nextLine();
+		User loggedInUser = serverInstance.login(UserID, Password);
+		
+		if (loggedInUser != null) {
+            System.out.println("Login successful. User: " + loggedInUser.getName());
+        } else {
+            System.out.println("Login failed. Please check your credentials.");
+        }
+		
+		
+		scanner.close();
 
 	}
 
